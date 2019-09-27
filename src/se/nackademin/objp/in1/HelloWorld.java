645c975f5b93b5ec5f9a10e88f3e34d941a2ba03
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2019-09-27 09:55:28 +0200

    Testar Hello World.

--- src/se/nackademin/objp/in1/HelloWorld.java
+++ src/se/nackademin/objp/in1/HelloWorld.java
@@ -1,8 +1,8 @@
 package se.nackademin.objp.in1;
 
-public class Main {
+public class HelloWorld {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello world");
     }
 }
