126bd9ce21171e1c62cc68f0007ce9ab119fade2
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2019-09-30 07:22:24 +0200

    Mer refaktorering.

--- src/se/nackademin/objp/in1/Main.java
+++ src/se/nackademin/objp/in1/Main.java
@@ -4,47 +4,61 @@ import javax.swing.*;
 
 public class Main {
 
-    public static Pet getPetByName(Pet[] searchArray, String name) {
-        Pet found = null;
-        for (Pet p : searchArray) {
-            if (name.equals(p.getName())) {
-                found = p;
-                break;
-            }
+    enum HotelGuests {
+        SIGGE(new Dog("Sigge", 5.0)),
+        DOGGE(new Dog("Dogge", 10.0)),
+        VENUS(new Cat("Venus", 5.0)),
+        OVE(new Cat("Ove", 3.0)),
+        HYPNO(new Snake("Hypno", 1.0));
+
+        private Pet namedPet;
+
+        private HotelGuests(Pet p) {
+            namedPet = p;
         }
-        // Exception om djuret inte hittas?
-        return found;
+
+        public Pet getPet() {
+            return namedPet;
+        }
+
+        public static Pet getPetByName(String name) {
+            for (HotelGuests p : HotelGuests.values()) {
+                if (name.equalsIgnoreCase(p.getPet().getName())) {
+                    return p.getPet();
+                }
+            }
+            return null;
+        }
     }
 
     public static void main(String[] args) {
-        Pet[] guests = {
-                new Dog("Sigge", 5.0),
-                new Dog("Dogge", 10.0),
-                new Cat("Venus", 5.0),
-                new Cat("Ove", 3.0),
-                new Snake("Hypno", 1.0)
-        };
-
         while (true) {
             // Visa inmatning
             try {
-                String lookup = JOptionPane.showInputDialog("Vilket djur ska få mat?");
-                // TODO Städa upp if-satserna
+                String lookup = JOptionPane.showInputDialog(
+                        "Vilket djur ska få mat?");
+                // TODO Städa upp metoden
                 if (lookup == null) {
                     break;
                 }
-                // Exception?
-                Pet toFeed = getPetByName(guests, lookup);
+                Pet toFeed = HotelGuests.getPetByName(lookup);
                 if (toFeed == null) {
                     // Djuret finns inte
+                    JOptionPane.showMessageDialog(null,
+                            "Hittade inget djur med namnet '" +
+                                    lookup + "'.");
                 } else {
                     double portion = toFeed.getOnePortion();
                     String foodType = toFeed.getFoodType();
                     // Skriv ut rekommendation
+                    JOptionPane.showMessageDialog(null,
+                            toFeed.getName() + " behöver " + portion
+                            + " gram " + foodType + " per portion.");
                 }
             } catch (Exception e) {
-                String errMsg = "Ett oväntat fel inträffade under körning:\n" +
-                        e.getLocalizedMessage();
+                String errMsg =
+                        "Ett oväntat fel inträffade under körning:\n" +
+                                e.getLocalizedMessage();
                 JOptionPane.showMessageDialog(null, errMsg);
             }
         }
