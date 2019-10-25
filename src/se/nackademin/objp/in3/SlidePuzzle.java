06533417d90c617b11aa2edaad52a3cc335dcb52
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2019-10-25 18:02:45 +0200

    Första grunden till inlämning 3

---
+++ src/se/nackademin/objp/in3/SlidePuzzle.java
@@ -0,0 +1,16 @@
+package se.nackademin.objp.in3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SlidePuzzle extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
