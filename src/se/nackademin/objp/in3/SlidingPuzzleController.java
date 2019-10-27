fbbe94bd584c955ec8f3f17fbfbabfaba9f4ade0
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2019-10-27 10:04:38 +0100

    Ett första utkast till controllern

---
+++ src/se/nackademin/objp/in3/SlidingPuzzleController.java
@@ -0,0 +1,26 @@
+package se.nackademin.objp.in3;
+
+public class SlidingPuzzleController {
+    SlidePuzzle view;
+    //SlidingPuzzleModel model;
+
+    // Lyssnare för nytt spel
+    // Lyssnare för inställningar
+    // Lyssnare för avslutning?
+
+    // Lyssnare för pusselbitar - de delar på en och identifieras
+    // med motsvarande bitar i modellen
+
+    SlidingPuzzleController() {
+        view = new SlidePuzzle();
+        //newGame(4, 4);
+    }
+
+    void newGame(int rows, int cols) {
+        //model = new SlidingPuzzleModel(rows, cols);
+        //Skapa GridPane här eller i vyn?
+        //view.setNågonting
+    }
+
+
+}
